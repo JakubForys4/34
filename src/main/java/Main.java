e09c534412f78218386962dbec3118b43a545b04
JakubForys4 <jakubforys09@gmail.com>
2025-04-02 15:11:46 +0000

    34

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,19 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Random;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int[]numbers = new int[10];
+    Random random = new Random();
+    for(int i=0;i<numbers.length;i++){
+      numbers[i]=random.nextInt(100);
+      
+    }
+    System.out.println("Tablica losowych liczb:");
+    for(int num:numbers){
+      System.out.print(num+" ");
+    }
+    
   }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+  
 }
